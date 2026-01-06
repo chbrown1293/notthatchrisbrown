1fb6584a4527ebd2e3537e99e8c6ba924b12aeb1
Chris Brown <chris.brown@advancedsymbolics.com>
2026-01-06 14:02:43 -0700

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { GoogleAnalytics } from "@next/third-parties/google";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -30,6 +31,7 @@ export default function RootLayout({
             >
                 {children}
             </body>
+            <GoogleAnalytics gaId="G-R77K50F7PK" />
         </html>
     );
 }
